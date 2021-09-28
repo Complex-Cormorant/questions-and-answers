0dbd4911ef74c347feb3e588c355e2bb029fcad3
Fikri Anuar <fikri101@gmail.com>
2021-09-28 18:16:05 -0400

    Completed many to many database

--- postgres-q-and-a-many-to-many.sql
+++ postgres-q-and-a-many-to-many.sql
@@ -17,6 +17,16 @@ CREATE TABLE answers(
   helpfulness SMALLINT DEFAULT 0,
   reported BOOLEAN NOT NULL DEFAULT false,
   email VARCHAR(60) NOT NULL,
-  photos VARCHAR [],
   question_id BIGINT NOT NULL REFERENCES questions (question_id)
  );
+
+CREATE TABLE photos(
+  photo_id BIGSERIAL NOT NULL PRIMARY KEY,
+  photo_url TEXT UNIQUE
+)
+
+CREATE TABLE answers_photos(
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  photo_id BIGINT NOT NULL REFERENCES photos (photo_id),
+  answer_id BIGINT NOT NULL REFERENCES answers (answer_id)
+)
