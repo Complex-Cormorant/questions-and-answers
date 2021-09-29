7a44ad0b1b0ac7519f995afe570efda22c4ba349
Fikri Anuar <fikri101@gmail.com>
2021-09-29 18:49:43 -0400

    Reorganized tables and added photos table

--- Postgres/postgres-q-and-a.sql
+++ Postgres/postgres-q-and-a.sql
@@ -1,22 +1,28 @@
 CREATE TABLE questions(
   question_id SERIAL NOT NULL PRIMARY KEY,
-  question_body VARCHAR(1000) TEXT NOT NULL,
+  product_id INT NOT NULL,
+  question_body VARCHAR(1000) NOT NULL,
   question_date DATE NOT NULL,
   asker_name VARCHAR(60) NOT NULL,
-  helpfulness SMALLINT DEFAULT 0,
-  reported BOOLEAN NOT NULL DEFAULT false,
   email VARCHAR(60) NOT NULL,
-  product_id INT NOT NULL
+  reported BOOLEAN NOT NULL DEFAULT false,
+  helpfulness SMALLINT DEFAULT 0
 );
 
 CREATE TABLE answers(
   answers_id SERIAL NOT NULL PRIMARY KEY,
+  question_id INT NOT NULL REFERENCES questions (question_id),
   answer_body VARCHAR(1000) NOT NULL,
   answer_date DATE NOT NULL,
   answerer_name VARCHAR(60) NOT NULL,
-  helpfulness SMALLINT DEFAULT 0,
-  reported BOOLEAN NOT NULL DEFAULT false,
   email VARCHAR(60) NOT NULL,
-  photos VARCHAR [5],
-  question_id INT NOT NULL REFERENCES questions (question_id)
+  reported BOOLEAN DEFAULT false,
+  helpfulness SMALLINT DEFAULT 0
+  photos VARCHAR [5]
+);
+
+CREATE TABLE photos(
+  photos_id SERIAL NOT NULL PRIMARY KEY,
+  answer_id INT NOT NULL REFERENCES answers (answers_id),
+  photo_url VARCHAR(2083) NOT NULL
 );
