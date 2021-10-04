b054118fdcb63a494270029707ec9d2edd0c05ac
Fikri Anuar <fikri101@gmail.com>
2021-10-04 13:17:11 -0400

    Added indexes

--- Postgres/etl.sql
+++ Postgres/etl.sql
@@ -5,3 +5,6 @@
 SELECT setval('questions_question_id_seq', (SELECT MAX(question_id) FROM questions));
 SELECT setval('answers_answer_id_seq', (SELECT MAX(answer_id) FROM answers));
 SELECT setval('photos_photo_id_seq', (SELECT MAX(photo_id) FROM photos));
+
+CREATE INDEX questions_product_id on questions(product_id);
+CREATE INDEX answers_question_id on answers(question_id);
