a7dca9902442607b88b699d1adb03ca78a4313f7
Fikri Anuar <fikri101@gmail.com>
2021-10-06 14:09:57 -0400

    Rearranged folders and added a config.example.js

---
+++ config.example..js
@@ -0,0 +1,12 @@
+const credentials = {
+  user: "EXAMPLE_USER",
+  host: "EXAMPLE_HOST",
+  database: "EXAMPLE_DATABASE",
+  password: "EXAMPLE_PASSWORD",
+  port: "EXAMPLE_PORT",
+};
+
+module.exports = credentials;
+
+// Make a copy of this file and rename it to config.js.
+// Change all configurations to match your PostgreSQL credentials.
